54163a97c48f8a63cb27961fff677417a3a6e2d5
alexesprit <alex.esprit@gmail.com>
2020-07-03 22:12:52 +0300

    Update rules to respect Prettier formatting

--- index.js
+++ index.js
@@ -55,8 +55,7 @@ module.exports = {
 		'string-quotes': 'single',
 		'unit-no-unknown': true,
 		'value-keyword-case': 'lower',
-		'value-list-comma-newline-after': 'always-multi-line',
-		'value-list-comma-space-after': 'always',
+		'value-list-comma-space-after': 'always-single-line',
 		'value-list-comma-space-before': 'never',
 		'value-list-max-empty-lines': 0,
 		'order/properties-alphabetical-order': true
